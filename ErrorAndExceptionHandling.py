678c81f71a8acde0f5fb3fb39848bdc05c458b14
athipornphumnicom <athiporn1990@gmail.com>
2021-02-15 08:19:51 +0700

    update

--- ErrorAndExceptionHandling.py
+++ ErrorAndExceptionHandling.py
@@ -1,24 +1,16 @@
-# https://docs.python.org/3/tutorial/errors.html
-# https://realpython.com/python-exceptions/
-# https://www.datacamp.com/community/tutorials/exception-handling-python
-# https://www.youtube.com/watch?v=NIWwJbo-9_8
-
 class NotAllowFileError(Exception):
     def __init__(self, filename):
-        self.message = filename + '  is not allowed.'
+        self.message = filename + ' is not allowed.'
         super().__init__(self.message)
 
 try:
-    # open a file, returns an error if the file does not exist
-    f1 = open('neis0736.txt')    
-    # create a file, returns an error if the file exist
-    f2 = open('abc.txt','x')    
-    # Custom exception
-    f3 = open('def.txt')
-    if f3.name == 'def.txt':
-        raise NotAllowFileError(f3.name)
-    # general exception
-    var = bad_var
+    f1 = open('neis0736.txt')
+    #f2 = open('abc.txt', 'x')
+    #f3 = open('def.txt')
+    #if f3.name == 'def.txt':
+    #    raise NotAllowFileError(f3.name)
+
+    #var = bad_var
 except FileNotFoundError as e:
     print(e)
 except FileExistsError as e:
@@ -32,3 +24,12 @@ else:
     f1.close()
 finally:
     print('End process')
+
+try:
+    pass
+except Exception as e:
+    raise
+else:
+    pass
+finally:
+    pass
